f74ff046bf3837599d7a181bd0241f1c91764e51
ciskiko <ciskiko@gmail.com>
2025-04-22 15:30:07 +0100

    Add files via upload

--- RegularWreathProduct.lean
+++ RegularWreathProduct.lean
@@ -1,3 +1,9 @@
+/-
+Copyright (c) 2025 Francisco Silva. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Francisco Silva
+-/
+
 import Mathlib.GroupTheory.Sylow
 
 /-!
